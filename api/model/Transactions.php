54ca58dfe96fc599b875e23abb201ce78729f2c2
joshkaye88 <jkaye@shaw.ca>
2022-11-07 23:08:49 -0800

    set up transactions page

--- api/model/Transactions.php
+++ api/model/Transactions.php
@@ -2,8 +2,42 @@
 
 class Transactions {
 
+  private $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
+  private $dbh;
+  private $stmt;
+
+
+
+
+  
+  public function __construct() {
+
+    $options = array(
+      PDO::ATTR_PERSISTENT => true,
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+
+    try {
+      $this->dbh = new PDO($this->dsn, DB_USER, DB_PASS, $options);
+    } catch(PDOException $err ) {
+      echo $err->getMessage();
+    }
+  }
+  
+
+
+
+
+
   public function add() {
-    // Post
+
+    // Format Data
+    $post_data = json_decode( file_get_contents("php://input") );
+
+    $users = new Users;
+    $user = $users->getUserById($post_data->userId);
+
+    
   }
 
   public function edit() {
