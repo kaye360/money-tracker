c390eded0f7e3ba71dfbb9f0e914ec88d43a3692
joshkaye88 <jkaye@shaw.ca>
2022-11-20 15:07:34 -0800

    update controller comments

--- api/controller.php
+++ api/controller.php
@@ -1,42 +1,53 @@
 <?php
 
+//
+// Class Controller
+// Receive GET/POST REQUEST, Get Model Class, and Render JSON
+//
+// Methods:
+// 
+// construct
+// render
+// parseURL
+//
 
 class Controller {
 
-  private $url;
+  //
+  // URL, class, method and params to run
+  //
+  private $url; 
   private $class;
   private $method;
   private $params;
 
-
-
-
-  
+  //
+  // Constructor  
+  // Determine what class/method/params to run based on REQUEST
+  //   
   public function __construct() {
 
-    // Current page URL
-    $this->url = $this->getURL();
+    $this->url = $this->parseURL();
     
-    // Check/Get Class
     if( !file_exists('./model/' . $this->url[0] . '.php')) throw new Exception('File/Class doesn\'t exist');
     $this->class = new $this->url[0];
 
-    // Check/Get Method
     if ( !isset($this->url[1])) throw new Exception('No method specified');
     $this->method = $this->url[1];
 
     if ( !method_exists($this->class, $this->method)) throw new Exception('Method doesn\'t exist');
     
-    // Check/Get Params. Params set to false on POST request
-    if( !isset($this->url[2]) && $_SERVER['REQUEST_METHOD'] == 'GET') throw new Exception('No Params specified on Get Rquest');
+    if( !isset($this->url[2]) && $_SERVER['REQUEST_METHOD'] == 'GET') {
+      throw new Exception('No Params specified on Get Rquest');
+    }
+
     $this->params = $_SERVER['REQUEST_METHOD'] == 'GET' ? $this->url[2] : false;
   
   }
-  
 
-
-
-  
+  // 
+  // Call method to return JSON to be rendered
+  // 
   public function render() {
 
     // Call class method with params
@@ -44,14 +55,12 @@ class Controller {
 
   }
 
-
-
-
-
+  //
   // Get URL and split into array
-  public function getURL() {
+  // Array
+  //
+  public function parseURL() {
     
-    // Check/Filter/Get URL
     if( !isset($_GET['url']) ) throw new Exception('No Url Param');
     $url = rtrim($_GET['url'], " \n\r\t\v\x00/");
     $url = filter_var($url, FILTER_SANITIZE_URL);
